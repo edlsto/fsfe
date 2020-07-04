fa2831fb3d5bc7867824e4b4e25e36789a111f10
Edward Stoner <edwardlstoner@gmail.com>
2020-07-04 09:09:03 -0600

    Add path

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-	res.send('Hello World!');
-
+app.get("/bad", (req, res) => {
+  res.status(418);
+  res.set("X-MyHeader", "Yo");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
